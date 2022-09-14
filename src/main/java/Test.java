1b5502e423914c8b264c548e992d7e80d726e1c2
poojithacn <poojitha.namini@freshworks.com>
2022-09-14 16:35:16 +0530

    First commit

---
+++ src/main/java/Test.java
@@ -0,0 +1,25 @@
+import java.io.*;
+
+class  Test {
+    public static void main (String[] args) throws IOException, ClassNotFoundException {
+        Save s1 = new Save();
+
+        s1.i = 90;
+        System.out.println(s1.i);
+
+        File f = new File("demo.txt");
+        FileOutputStream fos = new FileOutputStream(f);
+        ObjectOutputStream oos = new ObjectOutputStream(fos);
+        oos.writeObject(s1);
+
+        FileInputStream fis = new FileInputStream(f);
+        ObjectInputStream ois = new ObjectInputStream(fis);
+        Save s = (Save) ois.readObject();
+        System.out.println(s.i);
+
+    }
+}
+
+class Save implements Serializable {
+    int i;
+}
